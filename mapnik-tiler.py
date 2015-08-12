4028775c1d278df2037a6fd8d5c0dd53890bdc07
jon <sellars.jon@gmail.com>
2015-08-12 14:47:54 -0400

    Update mapnik-tiler.py

--- mapnik-tiler.py
+++ mapnik-tiler.py
@@ -3,7 +3,29 @@
 Based on:
 https://trac.openstreetmap.org/browser/applications/rendering/mapnik/generate_tiles_multiprocess.py
 With snippet from:
-gdal2tiles.py
+http://www.klokan.cz/projects/gdal2tiles/gdal2tiles.py
+
+The MIT License (MIT)
+
+Copyright (c) 2015 Jon Sellars
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 '''
 
 from math import pi,cos,sin,log,exp,atan
